31da423ea4ca59704d5c2757086153e05a860965
thekidjnr <thejnrkid@gmail.com>
2022-07-01 23:29:55 +0000

    q6

---
+++ src/components/Form/Q6/Q6.jsx
@@ -0,0 +1,52 @@
+import "./q6.css";
+
+const Q6 = () => {
+  return (
+    <section>
+      <div>
+        <h1>Question 6</h1>
+        <h2>
+          Which issues do you face with your business activities?This question
+          is required.
+        </h2>
+        <div>
+          <div>
+            <input
+              type="checkbox"
+              value="Challenges in receiving online payments"
+              id="1"
+            />
+            <label for="1">Challenges in receiving online payments</label>
+          </div>
+          <div>
+            <input
+              type="checkbox"
+              value="Delay in receiving payments from clients"
+              id="2"
+            />
+            <label for="2">Delay in receiving payments from clients</label>
+          </div>
+          <div>
+            <input
+              type="checkbox"
+              value="Advertisement of my business"
+              id="3"
+            />
+            <label for="3">Advertisement of my business</label>
+          </div>
+          <div>
+            <input
+              type="checkbox"
+              value="Difficulty in communicating with clients"
+              id="4"
+            />
+            <label for="4"> Difficulty in communicating with client</label>
+          </div>
+        </div>
+        <button>Next</button>
+      </div>
+    </section>
+  );
+};
+
+export default Q6;
--- src/pages/Form/form.jsx
+++ src/pages/Form/form.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import "./form.css";
+import Q6 from "../../components/Form/Q6/Q6";
 
 const Form = () => {
-  return <form> Form</form>;
+  return (
+    <main>
+      <form>
+        <Q6 />
+      </form>
+    </main>
+  );
 };
 
 export default Form;
