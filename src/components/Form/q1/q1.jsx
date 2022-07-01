fa83f0eab16e6751751b6927db6af148d1c33021
unknown <leonette.dapaah@ashesi.edu.gh>
2022-07-01 22:55:00 +0000

    edits

--- src/components/Form/q1/q1.jsx
+++ src/components/Form/q1/q1.jsx
@@ -29,8 +29,13 @@ const Q1 = () => {
             </div>
           </div>
       </div>
-      <button className="next__btn"  onClick={navigateToQ2}>
-        <p className="btn__text">Next</p>
+      <button className="next__btn" onClick={navigateToQ2}>
+        <p className="btn__text">Next &nbsp;<span className="vector">
+          <svg width="19" height="12" viewBox="0 0 19 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M9.50005 11.8L18.95 2.34999L17.375 0.87999L9.50005 8.64999L1.62505 0.87999L0.0500489 2.34999L9.50005 11.8Z" fill="white"/>
+          </svg>
+        </span>
+        </p>
       </button>
     </div>
   );
