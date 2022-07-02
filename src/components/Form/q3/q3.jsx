9df7f39b6749620d5d22ebc83b0835aaf380dc11
unknown <leonette.dapaah@ashesi.edu.gh>
2022-07-02 16:46:24 +0000

    edits

---
+++ src/components/Form/q3/q3.jsx
@@ -0,0 +1,48 @@
+import gadgets from "../../../photos/gadgets.jpg";
+
+const Q3 = () => {
+    return ( 
+        <div className="imq__body">
+            <div className="q__wrapper">
+                <h1>Question 3</h1>
+                <h2>Which gadget do you use the most for your business activities?</h2>
+                <div className="choice__wrapper">
+                    <div className="option">
+                        <input type="radio" value="Laptop" id="laptop" name="gadget"/>
+                        <label for="laptop">Laptop</label>
+                    </div>
+                    <div className="option">
+                        <input type="radio" value="Tablet" id="tablet" name="gadget"/>
+                        <label for="tablet">Tablet</label>
+                    </div>
+                    <div className="option">
+                        <input type="radio" value="Mobile Phone" id="phone" name="gadget"/>
+                        <label for="phone">Mobile Phone</label>
+                    </div>
+                </div>
+
+                <button className="next__btn">
+                    Next &nbsp;
+                    <span>
+                    <svg
+                        width="19"
+                        height="13"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <path
+                            d="M9.50005 11.8L18.95 2.34999L17.375 0.87999L9.50005 8.64999L1.62505 0.87999L0.0500489 2.34999L9.50005 11.8Z"
+                            fill="white"
+                        />
+                    </svg>
+                    </span>
+                </button>
+            </div>
+            <div className="img__wrapper">
+                <img src={gadgets} alt="gadgets" />
+            </div>
+        </div>
+     );
+}
+ 
+export default Q3;
--- src/pages/Form/form.jsx
+++ src/pages/Form/form.jsx
@@ -1,6 +1,9 @@
 import "./form.css";
 import Q1 from "../../components/Form/q1/q1";
 import Q2 from "../../components/Form/q2/q2";
+import Q3 from "../../components/Form/q3/q3";
+import Q4 from "../../components/Form/q4/q4";
+import Q5 from "../../components/Form/q5/q5";
 import Q6 from "../../components/Form/Q6/Q6";
 import Q7 from "../../components/Form/Q7/Q7";
 import Q8 from "../../components/Form/Q8/Q8";
@@ -13,6 +16,9 @@ const Form = () => {
       <form>
         <Q1 />
         <Q2 />
+        <Q3 />
+        <Q4 />
+        <Q5 />
         <Q6 />
         <Q7 />
         <Q8 />
