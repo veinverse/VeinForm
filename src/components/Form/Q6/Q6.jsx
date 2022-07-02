d830d3321f6390f058ae18315e9465d04842c816
thekidjnr <thejnrkid@gmail.com>
2022-07-02 01:54:54 +0000

    form default css

--- src/components/Form/Q6/Q6.jsx
+++ src/components/Form/Q6/Q6.jsx
@@ -2,57 +2,77 @@ import "./q6.css";
 
 const Q6 = () => {
   return (
-    <section>
-      <div>
-        <h1>Question 6</h1>
-        <h2>
-          Which issues do you face with your business activities?This question
-          is required.
-        </h2>
-        <div>
-          <div>
-            <input
-              type="checkbox"
-              value="Challenges in receiving online payments"
-              id="1"
-              name="business issues"
-            />
-            <label for="1">Challenges in receiving online payments</label>
-          </div>
-          <div>
-            <input
-              type="checkbox"
-              value="Delay in receiving payments from clients"
-              id="2"
-              name="business issues"
-            />
-            <label for="2">Delay in receiving payments from clients</label>
-          </div>
-          <div>
-            <input
-              type="checkbox"
-              value="Advertisement of my business"
-              id="3"
-              name="business issues"
-            />
-            <label for="3">Advertisement of my business</label>
-          </div>
-          <div>
-            <input
-              type="checkbox"
-              value="Difficulty in communicating with clients"
-              id="4"
-              name="business issues"
-            />
-            <label for="4">Difficulty in communicating with client</label>
-          </div>
-          <div>
-            <input type="text" placeholder="Other" name="business issues" />
-          </div>
+    <div className="q__wrapper">
+      <h1>Question 6</h1>
+      <h2>
+        Which issues do you face with your business activities?This question is
+        required.
+      </h2>
+      <div className="choice__wrapper">
+        <div className="option">
+          <input
+            type="checkbox"
+            value="Challenges in receiving online payments"
+            id="1"
+            name="business issues"
+          />
+          <label for="1">Challenges in receiving online payments</label>
         </div>
-        <button>Next</button>
+        <div className="option">
+          <input
+            type="checkbox"
+            value="Delay in receiving payments from clients"
+            id="2"
+            name="business issues"
+          />
+          <label for="2">Delay in receiving payments from clients</label>
+        </div>
+        <div className="option">
+          <input
+            type="checkbox"
+            value="Advertisement of my business"
+            id="3"
+            name="business issues"
+          />
+          <label for="3">Advertisement of my business</label>
+        </div>
+        <div className="option">
+          <input
+            type="checkbox"
+            value="Difficulty in communicating with clients"
+            id="4"
+            name="business issues"
+          />
+          <label for="4">Difficulty in communicating with client</label>
+        </div>
+        <div className="option">
+          <input
+            type="text"
+            placeholder="Other"
+            name="business issues"
+            className="text_enter"
+          />
+        </div>
       </div>
-    </section>
+      <button className="next__btn">
+        <p className="btn__text">
+          Next &nbsp;
+          <span>
+            <svg
+              width="19"
+              height="13"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M9.50005 11.8L18.95 2.34999L17.375 0.87999L9.50005 8.64999L1.62505 0.87999L0.0500489 2.34999L9.50005 11.8Z"
+                fill="white"
+              />
+            </svg>
+          </span>
+        </p>
+      </button>
+    </div>
   );
 };
 
--- src/components/Form/q1/q1.jsx
+++ src/components/Form/q1/q1.jsx
@@ -2,35 +2,30 @@ import "./q1.css";
 
 const Q1 = () => {
   return (
-    <div className="q1__wrapper">
-      <div className="question__text">
-        <h1 className="title">Question 1</h1>
-        <span className="question">What do you do?</span>
-      </div>
+    <div className="q__wrapper">
+      <h1>Question 1</h1>
+      <h2>What do you do?</h2>
       <div className="choice__wrapper">
-        <div className="answer__options">
-          <div className="option">
-            <div className="option__text">Event Planning</div>
-          </div>
-          <div className="option">
-            <div className="option__text">Own a Studio</div>
-          </div>
-          <div className="option">
-            <div className="option__text">Manage a Restaurant</div>
-          </div>
-          <div className="option">
-            <div className="option__text">Other</div>
-          </div>
+        <div className="option">
+          <div className="option__text">Event Planning</div>
+        </div>
+        <div className="option">
+          <div className="option__text">Own a Studio</div>
+        </div>
+        <div className="option">
+          <div className="option__text">Manage a Restaurant</div>
+        </div>
+        <div className="option">
+          <div className="option__text">Other</div>
         </div>
       </div>
       <button className="next__btn">
         <p className="btn__text">
           Next &nbsp;
-          <span className="vector">
+          <span>
             <svg
               width="19"
-              height="12"
-              viewBox="0 0 19 12"
+              height="13"
               fill="none"
               xmlns="http://www.w3.org/2000/svg"
             >
